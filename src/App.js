f10700a39bf5b5e2984f602710594f6ee3f40183
Lathryx <52394238+Lathryx@users.noreply.github.com>
2022-02-28 13:15:33 -0500

    added more food items, footer, and smooth scroll behavior

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
     <div>
       <div className="mx-auto w-11/12 sticky top-0 z-50 navbar bg-base-100 mb-20 shadow-xl rounded-xl">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">Bubonic Buffet</a>
+          <a href="#" className="btn btn-ghost normal-case text-xl">Bubonic Buffet</a>
         </div>
         <div className="flex-none">
           <div className="dropdown dropdown-end mr-5">
@@ -142,6 +142,11 @@ function App() {
           ))}
         </div> 
       </div>
+      <footer className="footer flex mx-auto p-4 w-11/12 bg-base-200 shadow-xl rounded-t-xl">
+              <p className="flex">Made with <svg xmlns="http://www.w3.org/2000/svg" className="m-0 p-0 h-5 w-5 text-primary" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+</svg> by <a href="https://github.com/lathryx" className="text-primary">Lathryx</a></p>
+      </footer>
     </div>
   );
 }
